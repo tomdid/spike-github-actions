2c7b22a670aef03e25fcd1c87852517de76218a6
Tom Did <hello@tomdid.com>
2021-02-02 08:24:53 +0000

    added extra test case to fail build

--- src/MyComponent.test.tsx
+++ src/MyComponent.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MyComponent from './MyComponent';
 
-test('renders learn react link', () => {
+test('Should render primary label', () => {
   const { getByText } = render(<MyComponent type="primary" />);
   const labelElement = getByText(/primary/i);
+  expect(labelElement).toBeInTheDocument();
+});
+
+test('Should throw error by purpose', () => {
+  const { getByText } = render(<MyComponent type="other" />);
+  const labelElement = getByText(/ERROR/i);
   expect(labelElement).toBeInTheDocument();
 });
